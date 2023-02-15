bd5a49f132942e14acf90ca6d625a3241a1944c8
Muhammad Anas <Anasnaseem420@gmail.com>
2023-02-16 03:00:48 +0500

    set-up

---
+++ app.js
@@ -0,0 +1,11 @@
+const btn = document.getElementById('btn');
+
+btn.addEventListener('click', () => {
+  while (true) {
+    console.log('Khatam');
+    console.log('Bye Bye');
+    console.log('Tataa');
+    console.log('Good Bye');
+    console.log('Gaya');
+  }
+})
